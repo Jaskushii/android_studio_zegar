ad372a31057e0bb3015d32b6fb052b520935fb53
WJ <wojcioch928@gmail.com>
2025-03-27 07:52:41 +0100

    dzialajacy alarm

--- app/src/main/java/com/example/zegar/MainActivity.java
+++ app/src/main/java/com/example/zegar/MainActivity.java
@@ -1,24 +1,53 @@
 package com.example.zegar;
 
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity {
+    private TextView alarmTextView;
+    private EditText timeInput;
+    private View viewBackground;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        alarmTextView = findViewById(R.id.alarm);
+        timeInput = findViewById(R.id.timeInput);
+        viewBackground = findViewById(R.id.zegar);
+
+        // Uruchomienie sprawdzania czasu co sekundę
+        handler.postDelayed(checkAlarmRunnable, 1000);
     }
+
+    private Runnable checkAlarmRunnable = new Runnable() {
+        @Override
+        public void run() {
+            String currentTime = new SimpleDateFormat("HH:mm", Locale.getDefault()).format(Calendar.getInstance().getTime());
+            String alarmTime = timeInput.getText().toString();
+
+            if (currentTime.equals(alarmTime)) {
+                alarmTextView.setText("ALARM!!!!");
+                viewBackground.setBackgroundColor(Color.RED);
+
+            } else {
+                alarmTextView.setText("NIC SIE NIE DZIEJE");
+                viewBackground.setBackgroundColor(Color.WHITE);
+            }
+
+            handler.postDelayed(this, 1000);
+        }
+    };
 }
